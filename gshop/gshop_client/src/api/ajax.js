4f17efd1f1fc3fa3260a6760319c09126d0a306e
captain <1083615314@qq.com>
2020-01-14 14:47:12 +0800

    ajax

---
+++ gshop/gshop_client/src/api/ajax.js
@@ -0,0 +1,35 @@
+/* 
+ajax请求模块 
+返回值：promise对象(异步返回的数据是：response.data)
+*/
+
+import axios from 'axios'
+
+export default function ajax(url,data = {},type = 'GET') {
+    return new Promise(function(resolve,reject) {
+        let promise;
+        if(type==='GET') {
+            // 准备url参数数据
+            let dataurl = '' //数据拼接字符串
+            Object.keys(data).forEach(item => {
+                dataurl = item + '=' + data[item] +'&';
+            })
+            if(dataurl!=='') {
+                dataurl = dataurl.substring(0,dataurl.lastIndexOf('&'));
+                url = url + '?' +dataurl
+            }
+            //发送get请求
+            promise = axios.get(url)
+        }else {
+            //发送post请求
+            promise = axios.post(url,data)
+        }
+        promise.then(res => {
+            //请求成功,调用resolve()
+            resolve(res.data);
+        }).catch(err => {
+            //请求失败，调用reject()
+            reject(err);
+        })
+    })
+}
